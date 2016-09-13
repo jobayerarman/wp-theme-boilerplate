a2b1abe2b106164f4ac88b4839ec805be31bfa9d
Jobayer Arman <carbonjha@gmail.com>
2016-09-13 14:24:38 +0600

    project init

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package wp-theme-boilerplate
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp-theme-boilerplate' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'wp-theme-boilerplate' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'wp-theme-boilerplate' ), 'wp-theme-boilerplate', '<a href="http://jobayerarman.github.io/" rel="designer">Jobayer Arman</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
