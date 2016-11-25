fb77708c128253d42e5d3d79c466179fbd08f3f8
Jobayer Arman <carbonjha@gmail.com>
2016-11-25 18:18:26 +0600

    update 1
    + variable format
    + production variable
    + more plugin
    + better error handling
    + cleanup tasks
    + conditonal style and script build tasks
    + browser reload fix
    + yarn package manager

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@
 // START Editing Project Variables.
 // Project related.
 var project              = 'WPThemeBoilerplate';                      // Project Name.
-var projectURL           = 'http://localhost/wp-theme-boilerplate';  // Project URL. Could be something like localhost:8888.
+var projectURL           = 'http://localhost/wp-theme-boilerplate';   // Project URL. Could be something like localhost:8888.
 var productURL           = './';                                      // Theme/Plugin URL. Leave it like it is, since our gulpfile.js lives in the root folder.
 
 // Translation related.
@@ -42,24 +42,31 @@ var lastTranslator       = 'Jobayer Arman <carbonjha@gmail.com>';     // Last tr
 var team                 = 'Jobayer Arman <carbonjha@email.com>';     // Team's Email ID.
 var translatePath        = './languages'                              // Where to save the translation files.
 
-// Style related.
-var styleSRC             = './assets/src/less/main.less';             // Path to main .less file.
-var styleDestination     = './assets/css/';                           // Path to place the compiled CSS file.
+// Style related
+var style = {
+  src    : './assets/src/less/main.less',             // Path to main .less file.
+  dest   : './assets/css/'                            // Path to place the compiled CSS file.
+};
 
-// JS Custom related.
-var scriptSRC            = './assets/src/js/*.js';                    // Path to JS custom scripts folder.
-var scriptDestination    = './assets/js/';                            // Path to place the compiled JS custom scripts file.
-var scriptFile           = 'script';                                  // Compiled JS custom file name.
+// JavaScript related
+var script = {
+  src    : './assets/src/js/*.js',                    // Path to JS custom scripts folder.
+  dest   : './assets/js/',                            // Path to place the compiled JS custom scripts file.
+  file   : 'script.js'                                   // Compiled JS custom file name.
+}
 
 // Images related.
-var imagesSRC            = './assets/src/img/**/*.{png,jpg,gif,svg}'; // Source folder of images which should be optimized.
-var imagesDestination    = './assets/img/';                           // Destination folder of optimized images. Must be different from the imagesSRC folder.
+var image = {
+  src    : './assets/src/img/**/*.{png,jpg,gif,svg}', // Source folder of images which should be optimized.
+  dest   : './assets/img/'                            // Destination folder of optimized images.
+}
 
 // Watch files paths.
-var styleWatchFiles      = './assets/src/less/**/*.less';             // Path to all *.scss files inside css folder and inside them.
-var scriptWatchFiles     = './assets/src/js/*.js';                    // Path to all custom JS files.
-var projectPHPWatchFiles = './**/*.php';                              // Path to all PHP files.
-
+var watch = {
+  style  : './assets/src/less/**/*.less',             // Path to all *.less files inside css folder and inside them.
+  script : './assets/src/js/*.js',                    // Path to all custom JS files.
+  php    : './**/*.php'                               // Path to all PHP files.
+}
 
 // Browsers you care about for autoprefixing.
 // Browserlist https://github.com/ai/browserslist
@@ -74,6 +81,12 @@ const AUTOPREFIXER_BROWSERS = [
   'opera >= 23',
   'safari >= 7',
 ];
+
+// production variable
+var config = {
+  production: !!gutil.env.production, // Two exclamations turn undefined into a proper false.
+  sourceMaps:  !gutil.env.production
+};
 // STOP Editing Project Variables.
 
 /**
@@ -88,11 +101,15 @@ var gutil        = require('gulp-util');             // Utility functions for gu
 var less         = require('gulp-less');             // Gulp pluign for Sass compilation.
 var cssmin       = require('gulp-cssmin');           // Minifies CSS files.
 var autoprefixer = require('gulp-autoprefixer');     // Autoprefixing magic.
+var sourcemaps   = require('gulp-sourcemaps');       // Maps code in a compressed file (E.g. style.css) back to it’s original position in a source file.
 
 // JS related plugins.
 var jshint       = require('gulp-jshint');           // JSHint plugin for gulp
 var concat       = require('gulp-concat');           // Concatenates JS files
 var uglify       = require('gulp-uglify');           // Minifies JS files
+
+// HTML template engine
+var processhtml  = require('gulp-processhtml');      // Process html files at build time to modify them depending on the release environment
 
 // Image realted plugins.
 var imagemin     = require('gulp-imagemin');         // Minify PNG, JPEG, GIF and SVG images with imagemin.
@@ -100,16 +117,60 @@ var imagemin     = require('gulp-imagemin');         // Minify PNG, JPEG, GIF an
 // Utility related plugins.
 var browserSync  = require('browser-sync').create(); // Reloads browser and injects CSS. Time-saving synchronised browser testing.
 var del          = require('del');                   // Delete files and folders
+var filter       = require('gulp-filter');           // Helps work on a subset of the original files by filtering them using globbing.
 var gulpSequence = require('gulp-sequence');         // Run a series of gulp tasks in order
-var errorHandler = require('gulp-plumber-notifier'); // Plumber then notify
+var gulpif       = require('gulp-if');               // A ternary gulp plugin: conditionally control the flow of vinyl objects.
+var lazypipe     = require('lazypipe');              // Lazypipe allows to create an immutable, lazily-initialized pipeline.
+var notify       = require('gulp-notify');           // Sends message notification to you
+var plumber      = require('gulp-plumber');          // Prevent pipe breaking caused by errors from gulp plugins
 var reload       = browserSync.reload;               // For manual browser reload.
 var rename       = require('gulp-rename');           // Renames files E.g. style.css -> style.min.css
 var size         = require('gulp-size');             // Logs out the total size of files in the stream and optionally the individual file-sizes
 var sort         = require('gulp-sort');             // Recommended to prevent unnecessary changes in pot-file.
 var wpPot        = require('gulp-wp-pot');           // For generating the .pot file.
 
+/**
+ * Notify Errors
+ */
+function errorLog(error) {
+  var lineNumber = (error.line) ? 'Line ' + error.line + ' -- ' : '';
+  var column     = (error.column) ? 'Col ' + error.column : '';
 
+  notify({
+    title: 'Task [' + error.plugin + '] Failed',
+    message: lineNumber + '' + column
+  }).write(error); //Error Notification
 
+  // Inspect the error object
+  // console.log(error);
+
+  // Pretty error reporting
+  var report = '';
+  var chalk = gutil.colors.white.bgRed;
+
+  report += '\n';
+  report += chalk('TASK:') + ' [' + error.plugin + ']\n';
+  report += chalk('PROB:') + ' ' + error.message + '\n';
+  if (error.lineNumber) { report += chalk('LINE:') + ' ' + error.lineNumber + '\n'; }
+  if (error.column) { report += chalk('COL:') + '  ' + error.column + '\n'; }
+  if (error.fileName)   { report += chalk('FILE:') + ' ' + error.fileName + '\n'; }
+  console.error(report);
+
+  this.emit('end');
+};
+
+/**
+ * Task: Cleanup
+ *
+ * Cleans destination files
+ */
+gulp.task('clean:css', function() {
+  return del([style.dest]);
+});
+gulp.task('clean:js', function() {
+  return del([script.dest]);
+});
+gulp.task('clean:all', gulpSequence('clean:css', 'clean:js'));
 
 /**
  * Task: `browser-sync`.
@@ -136,7 +197,7 @@ gulp.task( 'browser-sync', function() {
     injectChanges: true,
 
     // The small pop-over notifications in the browser are not always needed/wanted
-    notify: false,
+    notify: true,
   });
 });
 
@@ -147,27 +208,34 @@ gulp.task( 'browser-sync', function() {
  * Compiles LESS, Autoprefixes it and Minifies CSS.
  *
  */
-gulp.task('styles', function () {
-  return gulp.src( styleSRC )
-    .pipe( errorHandler() )
+var minifyCss = lazypipe()
+  .pipe(rename, {suffix: '.min'})
+  .pipe(cssmin, {keepSpecialComments: false});
 
-    .pipe( less() )
+gulp.task('styles', ['clean:css'], function() {
+  return gulp.src(style.src)
+    .pipe(plumber({errorHandler: errorLog}))
+    .pipe(gulpif(config.sourceMaps, sourcemaps.init()))
+
+    .pipe(less())
+
+    .pipe( gulpif(config.sourceMaps, sourcemaps.write({includeContent: false}))) // By default the source maps include the source code. Pass false to use the original files.
+    .pipe( gulpif(config.sourceMaps, sourcemaps.init({loadMaps: true})))         // Set to true to load existing maps for source files.
+
     .pipe( autoprefixer( AUTOPREFIXER_BROWSERS ) )
-    .pipe( gulp.dest( styleDestination ) )
-    .pipe( browserSync.stream() ) // Reloads style.css if that is enqueued.
-    .pipe( size({
-      showFiles: true
-    }) )
 
-    .pipe( rename( { suffix: '.min' } ) )
-    .pipe( cssmin({
-      keepSpecialComments: false
-    }))
-    .pipe( gulp.dest( styleDestination ) )
-    .pipe( browserSync.stream() ) // Reloads style.min.css if that is enqueued.
-    .pipe( size({
+    .pipe( gulpif(config.sourceMaps, sourcemaps.write('.')))
+
+    .pipe( gulpif(config.production, minifyCss()))
+
+
+    .pipe(gulp.dest(style.dest))
+    .pipe(filter('**/*.css'))                                                     // Filtering stream to only css files
+    .pipe(browserSync.stream())                                                   // Injects CSS into browser
+
+    .pipe(size({
       showFiles: true
-    }) );
+    }));
 });
 
 
@@ -177,28 +245,25 @@ gulp.task('styles', function () {
   * Concatenate and uglify custom JS scripts.
   *
   */
-gulp.task( 'scripts', function() {
-  return gulp.src( scriptSRC )
-    .pipe( errorHandler() )
+var minifyScripts = lazypipe()
+  .pipe(rename, {suffix: '.min'})
+  .pipe(uglify);
 
-    .pipe( jshint() )
-    .pipe( jshint.reporter('jshint-stylish') )
+gulp.task( 'scripts', ['clean:js'], function() {
+  return gulp.src(script.src)
+    .pipe(plumber({errorHandler: errorLog}))
 
-    .pipe( concat( scriptFile + '.js' ) )
-    .pipe( gulp.dest( scriptDestination ) )
-    .pipe( size({
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'))
+
+    .pipe(concat(script.file))
+    .pipe(gulpif(config.production, minifyScripts()))
+
+    .pipe(gulp.dest(script.dest))
+
+    .pipe(size({
       showFiles: true
-    }) )
-
-    .pipe( rename( {
-      basename: scriptFile,
-      suffix: '.min'
-    }))
-    .pipe( uglify() )
-    .pipe( gulp.dest( scriptDestination ) )
-    .pipe( size({
-      showFiles: true
-    }) );
+    }));
 });
 
 
@@ -209,14 +274,14 @@ gulp.task( 'scripts', function() {
   *
   */
 gulp.task( 'images', function() {
-  gulp.src( imagesSRC )
-  .pipe( imagemin( {
+  gulp.src(image.src)
+  .pipe(imagemin({
     interlaced: true,
     progressive: true,
     optimizationLevel: 5, // 0-7 low-high
     svgoPlugins: [{removeViewBox: false}]
-  } ) )
-  .pipe(gulp.dest( imagesDestination ));
+  }))
+  .pipe(gulp.dest(image.dest));
 });
 
 
@@ -229,33 +294,49 @@ gulp.task( 'images', function() {
   *     3. Applies wpPot with the variable set at the top of this file
   *     4. Generate a .pot file of i18n that can be used for l10n to build .mo file
   */
-gulp.task( 'translate', function () {
+gulp.task( 'translate', function() {
  return gulp.src( projectPHPWatchFiles )
-   .pipe( sort() )
-   .pipe( wpPot( {
-      domain        : text_domain,
-      destFile      : destFile,
-      package       : packageName,
-      bugReport     : bugReport,
-      lastTranslator: lastTranslator,
-      team          : team
-   } ))
-   .pipe( gulp.dest( translatePath ) );
+   .pipe(sort())
+   .pipe(wpPot({
+      domain         : text_domain,
+      destFile       : destFile,
+      package        : packageName,
+      bugReport      : bugReport,
+      lastTranslator : lastTranslator,
+      team           : team
+   }))
+   .pipe( gulp.dest(translatePath));
 });
 
 
 /**
  * Default Gulp task
  */
-gulp.task( 'default', gulpSequence( 'styles', 'scripts', 'images' ) );
+gulp.task( 'default', gulpSequence('clean:all', 'styles', 'scripts', 'translate', 'images'));
+
+/**
+ * Run all the tasks sequentially
+ * Use this task for development
+ */
+gulp.task( 'serve', gulpSequence('styles', 'scripts', 'watch'));
 
 /**
   * Watch Tasks.
   *
   * Watches for file changes and runs specific tasks.
   */
-gulp.task( 'serve', ['styles', 'scripts', 'browser-sync'], function () {
-  gulp.watch( projectPHPWatchFiles, reload ); // Reload on PHP file changes.
-  gulp.watch( styleWatchFiles, [ 'styles' ] ); // Reload on SCSS file changes.
-  gulp.watch( scriptWatchFiles, [ 'scripts', reload ] ); // Reload on script file changes.
+gulp.task('watch', ['styles', 'scripts', 'browser-sync'], function() {
+  gulp.watch(watch.style, ['styles']);         // Reload on less file changes.
+  gulp.watch(watch.php, ['watch-php']);        // Reload on PHP file changes.
+  gulp.watch(watch.script, ['watch-scripts']); // Reload on script file changes.
 });
+
+// reload browser
+gulp.task('watch-php', function(done) {
+  reload();
+  done();
+});
+gulp.task('watch-scripts', ['scripts'], function(done) {
+  reload();
+  done();
+})
