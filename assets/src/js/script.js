3b509c9e38b7be6cf2c7069fad5f22d907d71fc5
Jobayer Arman <carbonjha@gmail.com>
2016-11-27 22:07:42 +0600

    update 7
    + update js coding style
    + rename assets folder name

--- assets/src/js/script.js
+++
@@ -1,4 +0,0 @@
-$( document ).ready(function() {
-
-  // Write scripts here
-});
---
+++ assets/src/scripts/main.js
@@ -0,0 +1,4 @@
+(function($) {
+  // Load Events
+  $(document).ready();
+})(jQeuery); // Fully reference jQuery after this point.
